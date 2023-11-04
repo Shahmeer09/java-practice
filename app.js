b424ad6b274df5dfdbd7d9457b0fd0cb1dddcd75
Shahmeer09 <shahmeerrizwan921@gmail.com>
2023-11-04 20:22:17 +0500

    practice js

---
+++ app.js
@@ -0,0 +1,34 @@
+
+// let num = null
+// let putData = document.getElementById("display");
+
+// function generateTable(e){
+//   e.preventDefault()
+//   let i;
+//   for(i = 1; i<=10; i++){
+//     putData.innerHTML+=num+ " x " + i + " = " + num * i+"<br>";
+
+//   }
+//   let form = document.getElementById("num")
+//   form.value = null
+// }
+
+// function getNumber(e){
+//   num = e.target.value
+//   putData.innerHTML = null
+// }
+
+
+// for(i = 1; i<10; i++){
+//   console.log(i+1)
+// }
+
+// let i = 1;
+// while ( i <= 10 ){
+//   console.log(i);
+//   i++
+// }
+
+// const name = `shahmeer`;
+// const age = `19`
+// console.log(`my  name is ${name} and my age is ${age}`);
